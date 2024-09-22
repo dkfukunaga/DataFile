6e458647cdd39148d4aa778fa0d7cd7bd7e70b08
dkfukunaga <dkfukunaga@gmail.com>
2024-09-21 20:23:22 -0700

    tested TestItem by simply printing one to the console. looks good.

---
+++ doctest/testDriver.cpp
@@ -0,0 +1,14 @@
+
+
+#include "testItem.cpp"
+#include <iostream>
+
+int main() {
+    TestItem item_to_write;
+
+    std::cout << item_to_write.toString();
+
+    TestItem item_to_read;
+
+    return 0;
+}
